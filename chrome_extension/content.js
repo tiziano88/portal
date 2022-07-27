dda544fd24604d1e97252b1fea8479ce409226e4
Tiziano Santoro <tiziano88@gmail.com>
2022-07-27 21:00:17 +0100

    Highlight

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -5,8 +5,10 @@ async function portal_23947987492374987239479823749273() {
     var elements = document.getElementsByTagName('*');
     for (var i = 0; i < elements.length; i++) {
         var element = elements[i];
-        if (element.textContent == 'document'){
-            element.textContent = "FOO";
+        var replacement = replacements.get(element.innerText);
+        if (replacement){
+            element.textContent = replacement;
+            element.style.backgroundColor = 'red';
         }
     }
 }
@@ -14,17 +16,20 @@ async function portal_23947987492374987239479823749273() {
 async function portal_134248273948723984798237() {
     if (window.location.href.indexOf('https://github.com/') == 0) {
         var path = window.location.pathname;
-        var newPath = path.replace('/tree', '');
-        var url = 'https://raw.githubusercontent.com' + newPath + '/../portal';
+        var newPath = path.replace('/tree', '').replace('/blob', '');
+        var url = 'https://raw.githubusercontent.com' + newPath + '/../../portal';
         console.log('url: ' + url);
         var response = await fetch(url);
         var content = await response.text();
-        // console.log(content);
-        var replacements = new Map(content.split('\n').filter(line => line.trim() != "").map(line => {
-            var fields = line.split(' ');
-            return [fields[0], fields[1]];
-        }));
+        console.log(content);
+        var replacements = new Map(content.split('\n')
+            .filter(line => line.trim() != "")
+            .map(line => {
+                var fields = line.split(' ');
+                return [fields[0], fields[1]];
+            }));
         console.log(replacements);
+        return replacements;
     }
 }
 
