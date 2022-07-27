fd4111d000aa58344237ea9690c0925e5d07c755
Tiziano Santoro <tiziano88@gmail.com>
2022-07-27 20:54:06 +0100

    Replacements

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,6 +1,7 @@
 console.log('content.js loaded');
 
-function replace() {
+async function portal_23947987492374987239479823749273() {
+    var replacements = await portal_134248273948723984798237();
     var elements = document.getElementsByTagName('*');
     for (var i = 0; i < elements.length; i++) {
         var element = elements[i];
@@ -10,4 +11,21 @@ function replace() {
     }
 }
 
-window.onload = replace;
+async function portal_134248273948723984798237() {
+    if (window.location.href.indexOf('https://github.com/') == 0) {
+        var path = window.location.pathname;
+        var newPath = path.replace('/tree', '');
+        var url = 'https://raw.githubusercontent.com' + newPath + '/../portal';
+        console.log('url: ' + url);
+        var response = await fetch(url);
+        var content = await response.text();
+        // console.log(content);
+        var replacements = new Map(content.split('\n').filter(line => line.trim() != "").map(line => {
+            var fields = line.split(' ');
+            return [fields[0], fields[1]];
+        }));
+        console.log(replacements);
+    }
+}
+
+window.onload = portal_23947987492374987239479823749273;
